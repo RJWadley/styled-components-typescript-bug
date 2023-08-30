fe48dd671790e2bd2840f89c6b57ff59295455ce
Robbie <robbiethewadley@gmail.com>
2023-08-30 11:06:57 -0600

    simplify reproduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { GatsbyImage } from "gatsby-plugin-image";
-import { PerformanceMonitor } from "@react-three/drei";
 
 const Test = styled(GatsbyImage)<{ backgroundColor: string }>`
   background-color: ${(props) => props.backgroundColor};
